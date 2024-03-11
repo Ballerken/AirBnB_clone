6d381bb4523584a03be493d4e1d19e32d95fe77f
BlackOligarch <godfred.hammond2018@yahoo.com>
2024-03-11 13:48:26 +0000

    State Model - that inherits all attributes & model from parent class

---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""This module defines the State model
+   Imported from the parent class - Base Model
+"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Defines the State model."""
+
+    name = ""
