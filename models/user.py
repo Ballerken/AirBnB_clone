cc0bddec14735874f4c3612d1c18481bdc6aec68
BlackOligarch <godfred.hammond2018@yahoo.com>
2024-03-11 13:38:19 +0000

    User Model - that inherits all attributes & model from parent class

---
+++ models/user.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""This model define the User model
+   Imported from parent class - Base Model
+"""
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """Defines the User model."""
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
