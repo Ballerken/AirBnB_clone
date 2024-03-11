5f38994cfe67436655f9b2a9703d7e88f8815581
BlackOligarch <godfred.hammond2018@yahoo.com>
2024-03-11 14:01:30 +0000

    Added Amenity Model - that inherits all attributes & model from parent class

---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""This module defines the Amenity model
+   Imported from parent class - Base Model
+"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Defines the Amenity model"""
+
+    name = ""
